ef47dddc2ccceaae837bb4897402416c374e4441
Resse Christophe <christophe.resse@gmail.com>
2022-08-19 10:34:58 +0200

    chore(test): Re-factoring the unit tests.

--- cherry-base-utilities/src/test/java/com/hemajoo/commerce/cherry/base/utilities/test/helper/FileHelperUnitTest.java
+++ cherry-base-utilities/src/test/java/com/hemajoo/commerce/cherry/base/utilities/test/helper/FileHelperUnitTest.java
@@ -18,8 +18,7 @@ import com.hemajoo.commerce.cherry.base.utilities.helper.file.FileException;
 import com.hemajoo.commerce.cherry.base.utilities.helper.file.FileHelper;
 import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
@@ -32,7 +31,7 @@ import java.nio.file.Path;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * A test class for unit testing the {@link FileHelper} services.
+ * Test class for unit testing the {@link FileHelper} services.
  * @author <a href="mailto:christophe.resse@gmail.com">Christophe Resse</a>
  * @version 1.0.0
  */
@@ -40,31 +39,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FileHelperUnitTest extends BaseUnitTest
 {
     /**
-     * Sets up the test case.
+     * Test file.
      */
-    @BeforeEach
-    public final void setUpBeforeEach()
-    {
-        // Empty
-    }
+    private static final String FILE_CLASSPATH = "/log4j2-test.properties";
 
     /**
-     * Tears down the test case.
+     * Test file.
      */
-    @AfterEach
-    public final void tearDownAfterEach()
-    {
-        // Empty
-    }
+    private static final String FILE_URL = "https://www.w3.org/TR/PNG/iso_8859-1.txt";
 
     /**
-     * Dumps the content of a file.
+     * Test file.
+     */
+    private static final String FILE_JAR = "jar:file:./src/test/resources/google-auth-library-credentials-0.16.1.jar!/META-INF/MANIFEST.MF";
+
+    /**
+     * Test file.
+     */
+    private static final String FILE_FILE = "file:../doc/devops.md";
+
+    /**
+     * Dump the content of a file to the console.
      * @param filename File name.
-     * @param file File.
-     * @throws IOException Thrown in case an error occurred trying to dump the content of the file.
+     * @throws FileException Thrown in case an error occurred trying to find the file.
+     * @throws IOException Thrown in case an error occurred trying to access the file content.
      */
-    private static void dumpFile(final @NonNull String filename, final @NonNull File file) throws IOException
+    private static void dumpFile(final @NonNull String filename) throws IOException, FileException
     {
+        File file = FileHelper.getFile(filename);
+
         LOGGER.debug("File name is: ");
         LOGGER.debug(filename + "\n");
         LOGGER.debug("URL of file is: ");
@@ -73,170 +76,96 @@ class FileHelperUnitTest extends BaseUnitTest
         Files.lines(file.toPath(), StandardCharsets.UTF_8).forEach(LOGGER::debug);
     }
 
-    /**
-     * Test the {@link FileHelper#getFile(String)} service using a file loaded on the classpath.
-     */
     @Test
-    final void testFileHelperLoadFileFromClasspath() throws FileException
+    @DisplayName("Get a file from the classpath")
+    final void testGetFileFromClassPath() throws FileException, IOException
     {
-        String filename = "/log4j2-test.properties";
-
-        String content = FileHelper.loadFileContentAsString(filename);
-        assertThat(content).isNotNull();
-
-        LOGGER.debug(content);
-    }
-
-    /**
-     * Test the {@link FileHelper#loadFileContentAsString(String)} service using a file retrieved on the class path.
-     */
-    @Test
-    final void testFileHelperGetFileFromClasspath() throws FileException, IOException
-    {
-        String filename = "/log4j2-test.properties";
-
-        File file = FileHelper.getFile(filename);
+        File file = FileHelper.getFile(FILE_CLASSPATH);
+        LOGGER.debug(String.format("Getting file: %s from the classpath", FILE_CLASSPATH));
         assertThat(file).isNotNull();
-
-        dumpFile(filename, file);
     }
 
-    /**
-     * Test the {@link FileHelper#getFile(String)} service using a file located in a jar file on the class path.
-     */
     @Test
-    final void testFileHelperGetFileFromJar() throws FileException, IOException
+    @DisplayName("Get a file from a JAR file")
+    final void testGetFileFromJar() throws FileException
     {
-        String filename = "jar:file:/junit-4.13-beta-3.jar!/META-INF/MANIFEST.MF";
-        filename = "/changelog.txt";
-        filename = "/META-INF/MANIFEST.MF";
-
-        File file = FileHelper.getFile(filename);
+        File file = FileHelper.getFile(FILE_JAR);
+        LOGGER.debug(String.format("Getting file: %s from a JAR file", FILE_JAR));
         assertThat(file).isNotNull();
-
-        dumpFile(filename, file);
     }
 
-    /**
-     * Test the {@link FileHelper#loadFileContentAsString(String)} service using a file located in a jar file on the class path.
-     */
     @Test
-    final void testFileHelperLoadFileFromJar() throws FileException
+    @Timeout(10000)
+    @DisplayName("Get a file from an URL")
+    final void testGetFileFromUrl() throws FileException
     {
-        String filename;
-        filename = "/META-INF/MANIFEST.MF"; // Not unique, exist for many JAR files, first one is returned
-        filename = "jar:file:/Users/christophe/.m2/junit/junit/4.13-beta-3/junit-4.13-beta-3.jar!/META-INF/MANIFEST.MF";
-        filename = "/changelog.txt"; // Should be the changelog from lombok jar file.
-
-        String content = FileHelper.loadFileContentAsString(filename);
-        assertThat(content).isNotNull();
-
-        System.out.println(content);
-    }
-
-    /**
-     * Test the {@link FileHelper#getFile(String)} service using a file retrieved from an http url.
-     */
-    @Test()
-    @Timeout(30000) // 30 secs timeout
-    final void testFileHelperGetFileFromHttpUrl() throws FileException, IOException
-    {
-        String filename = "https://www.w3.org/TR/PNG/iso_8859-1.txt";
-
-        File file = FileHelper.getFile(filename);
+        File file = FileHelper.getFile(FILE_URL);
+        LOGGER.debug(String.format("Getting file: %s from an URL", FILE_URL));
         assertThat(file).isNotNull();
-
-        dumpFile(filename, file);
     }
 
-    /**
-     * Test the {@link FileHelper#loadFileContentAsString(String)} service using a file loaded from an http url.
-     */
-    @Test()
-    @Timeout(30000) // 30 secs timeout
-    final void testFileHelperLoadFileFromHttpUrl() throws FileException
-    {
-        String filename = "https://www.w3.org/TR/PNG/iso_8859-1.txt";
-
-        String content = FileHelper.loadFileContentAsString(filename);
-        assertThat(content).isNotNull();
-
-        System.out.println(content);
-    }
-
-    /**
-     * Test the {@link FileHelper#getFile(String)} service using a file retrieved from a file url.
-     */
     @Test
-    final void testFileHelperGetFileFromFileUrl() throws FileException, IOException
+    @DisplayName("Get a file from a file URL")
+    final void testGetFileFromFile() throws FileException
     {
-        // This test will fail if run on remote machine such as GitLab or Travis!
-        String filename = "file:/Volumes/Technology/project/gitlab.com/hemajoo/foundation/hemajoo-foundation/foundation-utility/target/test-classes/log4j2-test.properties";
-        filename = "file:../etc/deploy-settings.xml";
-
-        File file = FileHelper.getFile(filename);
+        File file = FileHelper.getFile(FILE_FILE);
+        LOGGER.debug(String.format("Getting file: %s from a file URL", FILE_FILE));
         assertThat(file).isNotNull();
-
-        dumpFile(filename, file);
     }
 
-    /**
-     * Test the {@link FileHelper#loadFileContentAsString(String)} service using a file loaded from a file url.
-     */
     @Test
-    final void testFileHelperLoadFileFromFileUrl() throws FileException
-    {
-        // This test will fail if run on remote machine such as GitLab or Travis!
-        String filename = "file:/Volumes/Technology/project/gitlab.com/hemajoo/foundation/hemajoo-foundation/foundation-utility/target/test-classes/log4j2-test.properties";
-        filename = "file:../etc/deploy-settings.xml";
-
-        String content = FileHelper.loadFileContentAsString(filename);
-        assertThat(content).isNotNull();
-
-        System.out.println(content);
-    }
-
-    /**
-     * Test the {@link FileHelper#getFile(String)} service using a file retrieved from a jar file url.
-     */
-    @Test
-    final void testFileHelperLoadFileFromJarUrl() throws FileException, IOException
-    {
-        String filename = "jar:file:/Users/christophe/.m2/junit/junit/4.13-beta-3/junit-4.13-beta-3.jar!/META-INF/MANIFEST.MF";
-        filename = "jar:file:./src/test/resources/google-auth-library-credentials-0.16.1.jar!/META-INF/MANIFEST.MF";
-
-        File file = FileHelper.getFile(filename);
-        assertThat(file).isNotNull();
-
-        dumpFile(filename, file);
-    }
-
-    /**
-     * Test the {@link FileHelper#loadFileContentAsString(String)} service using a file loaded from a jar file url.
-     */
-    @Test
-    final void testFileHelperGetFileFromJarUrl() throws FileException
-    {
-        String filename = "jar:file:/Users/christophe/.m2/junit/junit/4.13-beta-3/junit-4.13-beta-3.jar!/META-INF/MANIFEST.MF";
-        filename = "jar:file:./src/test/resources/google-auth-library-credentials-0.16.1.jar!/META-INF/MANIFEST.MF";
-
-        String content = FileHelper.loadFileContentAsString(filename);
-        assertThat(content).isNotNull();
-
-        System.out.println(content);
-    }
-
-    /**
-     * Test the ability to load a file using its file system complete path and name.
-     */
-    @Test
-    final void testFileHelperGetFileFromFileSystem() throws IOException, FileException
+    @DisplayName("Get a file from the file system")
+    final void testGetFileFromFileSystem() throws IOException, FileException
     {
         Path path = FileHelper.createTemporaryFile();
         File file = path.toFile();
-        file.deleteOnExit();
-        String content = FileHelper.loadFileContentAsString(file.getPath());
 
+        assertThat(file).isNotNull();
+        file.deleteOnExit();
+
+        String content = FileHelper.loadFileContentAsString(file.getPath());
+        LOGGER.debug(String.format("Loading file content: %s from the file system", file.getPath()));
+        assertThat(content).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Load a file from the classpath")
+    final void testLoadFileFromClassPath() throws FileException
+    {
+        String content = FileHelper.loadFileContentAsString(FILE_CLASSPATH);
+        LOGGER.debug(String.format("Loading file content: %s from the classpath", FILE_CLASSPATH));
+        assertThat(content).isNotNull();
+        assertThat(content).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Load a file from a JAR file")
+    final void testLoadFileFromJar() throws FileException
+    {
+        String content = FileHelper.loadFileContentAsString(FILE_JAR);
+        LOGGER.debug(String.format("Loading file content: %s from a JAR file", FILE_JAR));
+        assertThat(content).isNotNull();
+        assertThat(content).isNotNull();
+    }
+
+    @Test
+    @Timeout(10000)
+    @DisplayName("Load a file from an URL")
+    final void testLoadFileFromUrl() throws FileException
+    {
+        String content = FileHelper.loadFileContentAsString(FILE_URL);
+        LOGGER.debug(String.format("Loading file content: %s from an URL", FILE_URL));
+        assertThat(content).isNotNull();
+        assertThat(content).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Load a file from a file prefix")
+    final void testLoadFileFromFile() throws FileException
+    {
+        String content = FileHelper.loadFileContentAsString(FILE_FILE);
+        LOGGER.debug(String.format("Loading file content: %s from a file URL", FILE_FILE));
+        assertThat(content).isNotNull();
         assertThat(content).isNotNull();
     }
 }
